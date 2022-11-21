b3198ef3b5ea784afdb8f454263e66616a323aa6
Keenan-Kimbrough <keenankimbrough@gmail.com>
2022-11-20 20:29:34 -0500

    first commit

---
+++ contains-duplicate.py
@@ -0,0 +1,23 @@
+#Given an integer array nums, return true if any value appears at 
+# least twice in the array, and return false if every element is distinct.
+
+class Solution:
+    def containsDuplicate(self, nums) -> bool:
+        hashset = set()
+        
+        
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+            
+        return False
+    
+    
+    
+examplelist = [1,2,3,45,7,2]
+duplicateAlgorithm = Solution()
+ 
+print(duplicateAlgorithm.containsDuplicate(examplelist))
+            
+# this works correctly. O(n) Time complexity and O(n) space complexity.
